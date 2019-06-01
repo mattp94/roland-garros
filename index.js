33bddd1ac7c885de44d094b08a26daf9daa9ddeb
mattp94 <matthieu.puibaraud@isep.fr>
2019-06-01 12:08:17 +0200

    Add prettier

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
 
-const clipboardy = require('clipboardy')
-const enquirer = require('enquirer')
-const open = require('open')
-const { bold, green, magenta, red } = require('chalk')
-const { symbols } = require('ansi-colors')
+const clipboardy = require('clipboardy');
+const enquirer = require('enquirer');
+const open = require('open');
+const { bold, green, magenta, red } = require('chalk');
+const { symbols } = require('ansi-colors');
 
-const getId = require('./lib/id')
-const getLives = require('./lib/live')
-const getStream = require('./lib/stream')
-const getToken = require('./lib/token')
+const getId = require('./lib/id');
+const getLives = require('./lib/live');
+const getStream = require('./lib/stream');
+const getToken = require('./lib/token');
 
 const main = async () => {
-    const lives = await getLives()
+    const lives = await getLives();
 
     if (!lives.length) {
-        throw new Error('Aucun direct disponible.')
+        throw new Error('Aucun direct disponible.');
     }
 
     const { title, app } = await enquirer.prompt([
@@ -23,40 +23,40 @@ const main = async () => {
             type: 'autocomplete',
             name: 'title',
             message: 'Which live do you wanna watch?',
-            choices: lives.map(live => live.title)
+            choices: lives.map(live => live.title),
         },
         {
             type: 'autocomplete',
             name: 'app',
             message: 'Which app do you wanna use?',
-            choices: ['QuickTime Player', 'Safari', 'VLC', 'Clipboard']
-        }
-    ])
+            choices: ['QuickTime Player', 'Safari', 'VLC', 'Clipboard'],
+        },
+    ]);
 
-    const { url } = lives.find(live => live.title === title)
-   
-    const id = await getId(url)
-    const token = await getToken(id)
-    const stream = await getStream(token)
+    const { url } = lives.find(live => live.title === title);
+
+    const id = await getId(url);
+    const token = await getToken(id);
+    const stream = await getStream(token);
 
     if (app === 'Clipboard') {
-        await clipboardy.write(stream)
-        
+        await clipboardy.write(stream);
+
         console.log(
             green(symbols.check),
-            bold('It has been copied to the Clipboard!')
-        )
+            bold('It has been copied to the Clipboard!'),
+        );
     } else {
-        await open(stream, { app })
+        await open(stream, { app });
     }
-}
+};
 
 main().catch(err => {
     if (err instanceof Error) {
         console.error(
             red(symbols.cross),
             bold('An error occurred:'),
-            magenta(err.message)
-        )
+            magenta(err.message),
+        );
     }
-})
+});
